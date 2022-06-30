6039ff2b5ba6cb89849f93db827bda04bcb9688f
Nima <lamap8708@gmail.com>
2022-06-30 09:57:11 +0545

    edit

--- scripts/rps.js
+++ scripts/rps.js
@@ -14,18 +14,39 @@ function playerPlay() {
     return pplay;
 };
 
-var playerSelection = playerPlay();
-var computerSelection = computerPlay();
-console.log(playerSelection, computerSelection);
 
-function result(playerSelection, computerSelection) {
+var playerWinCount = 0;
+var computerWinCount = 0;
+
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === "scissor" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissor" || playerSelection === "rock" && computerSelection === "paper") {
-        console.log("computer wins! " + computerSelection + " beats " + playerSelection + ".");
+        console.log(playerSelection, computerSelection);
+        computerWinCount = computerWinCount + 1;
+        console.log(playerWinCount, computerWinCount);
+        return ("computer wins! this round. " + computerSelection + " beats " + playerSelection + ".");
     } else if (playerSelection === computerSelection) {
-        console.log("nobody wins! Draw");
+        console.log(playerSelection, computerSelection);
+        console.log(playerWinCount, computerWinCount);
+        return ("nobody wins! this round Draw");
     } else {
-        console.log("you win! " + playerSelection + " beats " + computerSelection + ".");
+        console.log(playerSelection, computerSelection);
+        playerWinCount = playerWinCount + 1;
+        console.log(playerWinCount, computerWinCount);
+        return ("you win! this round " + playerSelection + " beats " + computerSelection + ".");
     };
-};
+    };
 
-result(playerSelection, computerSelection);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerPlay(), computerPlay()));
+    };
+    if (playerWinCount > computerWinCount) {
+        console.log("you win this game.");
+    } else if (playerWinCount < computerWinCount) {
+        console.log("you lose!");
+    } else {
+        console.log("Draw");
+    };
+}
+
+game();
